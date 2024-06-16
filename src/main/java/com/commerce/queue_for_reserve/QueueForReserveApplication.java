935aba5f097704ab8c844c670c35c780b2fcbe00
Shinnybest <kim295521@naver.com>
2024-06-16 21:53:10 +0900

    initial commit

---
+++ src/main/java/com/commerce/queue_for_reserve/QueueForReserveApplication.java
@@ -0,0 +1,13 @@
+package com.commerce.queue_for_reserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueueForReserveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueueForReserveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/commerce/queue_for_reserve/QueueForReserveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.commerce.queue_for_reserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueueForReserveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
