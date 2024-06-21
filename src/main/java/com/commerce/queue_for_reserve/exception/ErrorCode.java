a4be5475ea835a5652b6c8a74cbb8c5728a98317
Shinnybest <kim295521@naver.com>
2024-06-21 17:09:57 +0900

    [ISSUE-#21] 대기열의 순서 반환하는 통합 테스트 코드 작성

--- src/main/java/com/commerce/queue_for_reserve/exception/ErrorCode.java
+++ src/main/java/com/commerce/queue_for_reserve/exception/ErrorCode.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 
 @AllArgsConstructor
 public enum ErrorCode {
-    USER_NOT_IN_PROGRESS(HttpStatus.FORBIDDEN, "001", "This user is not in the processing queue.");
-
+    USER_NOT_IN_PROGRESS(HttpStatus.FORBIDDEN, "001", "This user is not in the processing queue."),
+    FAILED_TO_ADD_IN_WAIT_QUEUE(HttpStatus.INTERNAL_SERVER_ERROR, "001", "Not added in the waiting queue.");
     private final HttpStatus httpStatus;
     private final String code;
     private final String reason;
